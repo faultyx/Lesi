8cf977e49a65d114164a2a39de82b80ba01314db
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-20 11:46:07 +0100

    Add files via upload

---
+++ utils/Sqlite.js
@@ -0,0 +1,16 @@
+const sql = require("sqlite3"),
+chalk = require("chalk"),
+db = new sql.Database("lesi_db.sqlite");
+console.log(chalk.grey("[SQLITE]:") + " Initialised.");
+
+const tables = {
+  guild: [
+    "id INTEGER NOT NULL",
+    "prefix TEXT NOT NULL"
+  ]
+}
+  for(let table in tables) {
+  	db.run(`CREATE TABLE ${table} (${tables[table].join(", ")})`, () => {
+  		console.log(chalk.blue("[SQLITE: TABLE: CREATE]:") + ` ${table}`);
+  	})
+  }
