419029ba4ba7e4b04c896e66f32f3e330c52f9a1
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-21 15:23:27 +0100

    blacklists, cmd etc..

--- events/message.js
+++ events/message.js
@@ -1,8 +1,12 @@
 const keys = require("../keys"),
 ids = require("../ids");
+
+const db = require("../utils/Sqlite");
 module.exports.run = async (client, msg) => {
 
   if (msg.author.bot) return;
+
+  if (msg.guild && !msg.member) await msg.guild.fetchMember(msg.author);
 
   const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
   const prefix = msg.content.match(prefixMention) ? msg.content.match(prefixMention)[0] : keys.defaultPrefix;
@@ -11,9 +15,59 @@ module.exports.run = async (client, msg) => {
 
   const args = msg.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+  db.get(`SELECT * FROM userblacklist WHERE snowflake = ?`, msg.author.id, (err, r) => {
+      if (err) console.log(err);
+      if (r) return msg.reply(`You are blacklisted from the bot for reason: \`${r.reasonOfBlacklist}\` | You can contact the developer or staff in: ${ids.support_server} to make a appeal!`)
+    });
+    // guild blacklist coming soon.
 
   const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command));
 
-  cmd.run(client, msg, args, ids, keys);
+  if (!cmd) return;
+
+  if (cmd.conf.ownerOnly && msg.author.id !== msg.guild.owner.user.id) {
+    return msg.channel.send({
+      embed: {
+        color: 0xff3e28,
+        timestamp: new Date(),
+        title: "Insufficient Permissions",
+        description: `\`CMD: ${cmd.help.nam}\`\nThis command can only be used by the server owner.`,
+        footer: {
+          text: msg.author.tag,
+          icon_url: msg.author.displayAvatarURL
+        }
+      }
+    });
+  };
+  if (cmd.conf.developerOnly && msg.author.id !== keys.developerID) {
+    return msg.channel.send({
+      embed: {
+        color: 0xff3e28,
+        timestamp: new Date(),
+        title: "Insufficient Permissions",
+        description: `\`CMD: ${cmd.help.nam}\`\nThis command can only be used by the developer.`,
+        footer: {
+          text: msg.author.tag,
+          icon_url: msg.author.displayAvatarURL
+        }
+      }
+    });
+  };
+  if (cmd.conf.guildOnly && !msg.guild) {
+    return msg.channel.send({
+      embed: {
+        color: 0xff3e28,
+        timestamp: new Date(),
+        title: "Command Run Error",
+        description: `\`CMD: ${cmd.help.nam}\`\nThis command can only be used in a server.`,
+        footer: {
+          text: msg.author.tag,
+          icon_url: msg.author.displayAvatarURL
+        }
+      }
+    });
+  };
+
+  cmd.run(client, msg, args, ids, keys, db);
 
 };
