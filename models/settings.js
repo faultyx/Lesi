5d4de1abe55288f97e5743c948994331e01ed6cc
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-24 11:54:39 +0100

    { defaultPrefix }

--- models/settings.js
+++ models/settings.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const { defaultPrefix } = require("../keys");
 
 const settingSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   guildID: String,
   prefix: {
     type: String,
-    default: ">"
+    default: defaultPrefix
   }
 });
 
