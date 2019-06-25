cf2cee1fa201c8d16bf910668d270f0f1842d2fe
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-25 11:53:24 +0100

    Add files via upload

--- models/settings.js
+++ models/settings.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const { defaultPrefix } = require("../keys");
 
 const settingSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   guildID: String,
   prefix: String,
   modules: {
