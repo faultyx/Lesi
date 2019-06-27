97fe0301ddf1ea743b7bfd5891dfd1c9a57e1236
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-27 12:19:00 +0100

    Add files via upload

---
+++ commands/bird.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const ns = require("node-superfetch");
+module.exports = {
+  execute: async (client, msg, args) => {
+  try {
+  const { body } = await ns
+  .get("https://shibe.online/api/birds")
+  .query({
+    count: 1,
+    urls: true,
+    httpsUrls: true
+  });
+  return msg.channel.send(`**Requested by:** ${msg.author.toString()}`, {
+    embed: {
+      color: 0xffffff,
+      timestamp: new Date(),
+      image: {
+        url: body[0]
+      }
+    }
+  });
+} catch (error) {
+  console.log(error);
+  return msg.channel.send("There was an error trying to get a bird image.");
+}
+
+  }
+};
+module.exports.help = {
+  nam: "bird",
+  mod: "fun",
+  desc: "Get a bird image!",
+  usa: "[prefix]bird",
+  exa: [">bird"]
+};
+module.exports.conf = {
+  aliases: ["birdy"],
+  guildOnly: false,
+  ownerOnly: false,
+  developerOnly: false,
+  hiddenFromHelp: false
+};
