b204c05ed3a57a4a491a0b70d08cc29480808a9f
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-27 12:22:43 +0100

    Add files via upload

---
+++ commands/dog.js
@@ -0,0 +1,37 @@
+const sa = require("superagent");
+module.exports = {
+  execute: async (client, msg, args) => {
+  sa.get("https://random.dog/woof.json")
+  .end((err, res) => {
+  if (!err && res.status === 200) {
+    return msg.channel.send(`**Requested by:** ${msg.author.toString()}`, {
+      embed: {
+        color: 0xffffff,
+        timestamp: new Date(),
+        image: {
+          url: res.body.url
+        }
+      }
+    });
+  } else {
+    return msg.channel.send("There was an error trying to get a bird image.");
+  }
+
+  });
+
+  }
+};
+module.exports.help = {
+  nam: "dog",
+  mod: "fun",
+  desc: "Get a dog image!",
+  usa: "[prefix]dog",
+  exa: [">dog"]
+};
+module.exports.conf = {
+  aliases: ["doggy"],
+  guildOnly: false,
+  ownerOnly: false,
+  developerOnly: false,
+  hiddenFromHelp: false
+};
