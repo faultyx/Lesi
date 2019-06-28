f05b12a3e026653e5c44a97d3f5e54b58cf007c9
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-06-28 11:33:15 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -22,7 +22,7 @@ module.exports = {
             },
             {
               name: "Guild:",
-              value: "\`prefix\`"
+              value: "\`prefix, description\`"
             }
           ]
         }
