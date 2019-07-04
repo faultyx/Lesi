eefad253c68bad1ee7835e8aff7ff0783b91a30e
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-04 12:05:06 +0100

    fix

--- events/reconnecting.js
+++ events/reconnecting.js
@@ -1,6 +1,6 @@
 const { green } = require("chalk");
 module.exports = {
   execute(client, guild) {
-  console.log(green("[RECONNECTING: AT]:") + ` ${new Date()}`);
+  console.log(green("[RECONNECTING: AT]: ") + new Date());
   }
 };
