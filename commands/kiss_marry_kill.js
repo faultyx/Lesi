9ba71629d0e7c76246b0ee04c7256053d00c231a
Faulty X <40147859+faultyx@users.noreply.github.com>
2019-07-22 11:16:18 +0100

    Update kiss_marry_kill.js

--- commands/kiss_marry_kill.js
+++ commands/kiss_marry_kill.js
@@ -15,7 +15,7 @@ module.exports = {
       for (let i = arr.length - 1; i >= 0; i--) {
       const m = Math.floor(Math.random() * (i + 1));
       const temp = arr[i];
-      arr[i] = arr[j];
+      arr[i] = arr[m];
       arr[m] = temp;
       }
       return arr;
